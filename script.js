6f1c068b3444d46cceb2bfc51237d3daf5b2e487
KhyatiBhosle <56262867+KhyatiBhosle@users.noreply.github.com>
2023-11-02 14:24:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 var head = document.head;
 var link = document.createElement("link");
+var letters = document.querySelectorAll(".single");
+var textBox = document.querySelector(".textbox");
+var space = document.getElementById("space");
+var backspace = document.getElementById("backspace");
+
+function upper(){
+    letters.forEach(letter => {
+        if (textBox.textContent == ""){
+            letter.innerHTML = letter.innerHTML.toUpperCase();
+        }
+        else if(textBox.textContent[textBox.textContent.length - 1] == " " && textBox.textContent[textBox.textContent.length - 2] == "."){
+            letter.innerHTML = letter.innerHTML.toUpperCase();
+        }
+        else{
+            letter.innerHTML = letter.innerHTML.toLowerCase();
+        }
+    });
+}
 
 link.rel = "stylesheet";
 link.href = "style-dark.css";
@@ -14,3 +32,26 @@ Switch.addEventListener('change', function(){
         head.removeChild(link);
     }
 });
+
+textBox.textContent = null;
+
+upper();
+
+letters.forEach(letter => {
+    letter.addEventListener('click', function(){
+        textBox.textContent = textBox.textContent + letter.innerHTML;
+        upper();
+    })
+});
+
+
+space.addEventListener('click', function(){
+    textBox.textContent = textBox.textContent + " ";
+    upper();
+});
+
+
+backspace.addEventListener('click', function(){
+    textBox.textContent = textBox.textContent.slice(0, -1);
+    upper();
+})
